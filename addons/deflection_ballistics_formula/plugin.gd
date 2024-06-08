154f30390d7ec024a4bc9dcdacd4fc368c12efb4
neclor <al.pavlov2012@gmail.com>
2024-06-08 15:33:17 +0200

    rename folder

--- addons/deflection_ballistics_formula/plugin.gd
+++ addons/deflection_ballistics_formula/plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-	add_autoload_singleton("Deflection", "res://addons/deflection-ballistics-formula/deflection-ballistics-formula.gd")
+	add_autoload_singleton("Deflection", "res://addons/deflection_ballistics_formula/deflection_ballistics_formula.gd")
 
 
 func _exit_tree() -> void:
